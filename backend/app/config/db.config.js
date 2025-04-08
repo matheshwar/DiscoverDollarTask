63a704c354a78b2940424c0d23eeb1e9d8faca79
Mathesh <matheshwar.srinivasan@gmail.com>
2025-04-08 14:37:44 +0530

    Update db.config.js

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,6 +1,6 @@
- #module.exports = {
- #url: "mongodb://localhost:27017/dd_db"
-#};
+ //module.exports = {
+ //url: "mongodb://localhost:27017/dd_db"
+//};
 module.exports = {
   url: "mongodb://mongodb:27017/dd_db"
 };
